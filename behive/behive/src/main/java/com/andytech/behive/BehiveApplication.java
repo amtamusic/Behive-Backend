184034ee60e3662a9d3a3ed9f5d2ba98afc60d3b
Andres Toledo <amtamusic@users.noreply.github.com>
2021-06-09 16:16:22 -0400

    Created Project
    Created Spring Boot Applicationi

---
+++ behive/behive/src/main/java/com/andytech/behive/BehiveApplication.java
@@ -0,0 +1,13 @@
+package com.andytech.behive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BehiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BehiveApplication.class, args);
+	}
+
+}
---
+++ behive/behive/src/test/java/com/andytech/behive/BehiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andytech.behive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BehiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
